a391008d63072fb6fc7c6113be2823ad7b7a7472
X1r0z <i@exp10it.cn>
2019-08-11 15:34:36 +0800

    add backup

---
+++ docs/backup.py
@@ -0,0 +1,40 @@
+import requests
+import time
+import json
+import sys
+import os
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+folder = 'posts/'
+
+if not os.path.exists(folder):
+	os.mkdir(folder)
+
+url = 'https://api.github.com/repos/X1r0z/exp10it/issues'
+
+headers = {
+	'Accept': 'application/vnd.github.v3+json',
+	'Authorization': 'token a5cef10acab3f91e535bce45ad7f9d2b16ad972e'
+}
+
+p = 1
+
+while True:
+	params = {
+	'filter': 'created',
+	'page': p
+	}
+	response = requests.get(url, params=params, headers=headers)
+	data = json.loads(response.text)
+	if not data:
+		break
+	for item in data:
+		title, rawdate = item['title'].split(' #')
+		body = item['body']
+		date = time.strftime('%Y-%m-%d', time.strptime(rawdate, '%b %d, %Y'))
+		with open (folder + date + ' ' + title + '.md', 'w+') as f:
+			f.write(body)
+		print title,'OK'
+	p += 1
