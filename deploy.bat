954354dd9bd5c3bd568b328b94075e0d9e2c54c0
X1r0z <i@exp10it.cn>
2023-04-13 17:22:21 +0800

    auto deploy

--- deploy.bat
+++
@@ -1,7 +0,0 @@
-rmdir public /S /Q
-rmdir resources /S /Q
-del .hugo_build.lock
-git add .
-git commit -m "auto deploy"
-git push
-pause
---
+++ deploy.sh
@@ -0,0 +1,5 @@
+rm -rf resources/
+rm .hugo_build.lock
+git add .
+git commit -m 'auto deploy'
+git push
